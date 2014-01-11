31de61232c29c44e9d48424955a15da54767978d
assafp <assafp@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-01-11 15:47:37 +0000

    6.5.0
    git-svn-id: https://plugins.svn.wordpress.org/social-polls-by-opinionstage/trunk@836719 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- opinionstage-functions.php
+++ opinionstage-functions.php
@@ -134,8 +134,8 @@ function opinionstage_add_poll_page() {
 		  <h2>Actions</h2>
 		  <ul class="os_links_list">
 			<li><?php echo opinionstage_create_link('Create a Poll', 'new_poll', ''); ?></li>
-			<li><?php echo opinionstage_create_link('Manage Your Polls', 'dashboard', ''); ?></li>
-			<li><a href="http://www.opinionstage.com/dashboard?tab=containers" target="_blank">Manage Your Containers</a></li>
+			<li><?php echo opinionstage_create_link('Manage Polls', 'dashboard', ''); ?></li>
+			<li><a href="http://www.opinionstage.com/dashboard?tab=containers" target="_blank">Manage Containers</a></li>
 		  </ul>
 		  <h2>Help</h2>
 		  <ul class="os_links_list">			
